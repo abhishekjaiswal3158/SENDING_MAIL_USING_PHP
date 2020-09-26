614d133e39f124af6b63960ef25cafec86358bd8
Aditya Gupta <ag15035@gmail.com>
2020-09-26 12:17:42 +0530

    Update SENDING_MAIL_USING_PHP.php

--- SENDING_MAIL_USING_PHP.php
+++ SENDING_MAIL_USING_PHP.php
@@ -6,7 +6,7 @@
 
     $body = $_POST["MESSAGE"];
 
-    $headers = "From: ".$_POST["FROM"];
+    $headers = "From: " . $_POST["FROM"];
 
     if (mail($emailTo, $subject, $body, $headers)) {
         
@@ -17,7 +17,6 @@
         echo "The email could not be sent.";
         
     }
-
 
 ?>
 <!doctype html>
@@ -38,16 +37,16 @@
        
    <div class="form-group">
     <label for="FROM">Email From</label>
-    <input type="email" class="form-control" name="FROM" placeholder="name@example.com">
+    <input type="email" class="form-control" name="FROM" placeholder="from@some.in">
   </div>
   <div class="form-group">
     <label for="TO">Email To</label>
-    <input type="email" class="form-control" name="TO" placeholder="name@example.com">
+    <input type="email" class="form-control" name="TO" placeholder="to@some.in">
   </div>
 
   <div class="form-group">
-    <label for="SUBJECT">Subject</label>
-    <input type="text" class="form-control" name="SUBJECT" ">
+    <label for="sub">Subject</label>
+    <input type="text" class="form-control" name="sub">
   </div>
  
   <div class="form-group">
@@ -58,11 +57,5 @@
     <input type="submit" class="form-control" name="submit">
   </div>
 </form>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
 </html>
